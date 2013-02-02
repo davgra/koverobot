4e77ce8ec212938a02d47da80bf864fbf25f136c
David Granath <dur@musiker.nu>
2013-02-02 10:15:08 +0100

    Sites keep stock of goods

--- README.md
+++ README.md
@@ -16,16 +16,27 @@ unless you use rvm, in which case you just run
 ## Gameplay
 
 In the game world there is a number of factories and cities. These
-prpduce and/or consume commodities. There is a number of robot vehicles
+produce and/or consume commodities. There is a number of robot vehicles
 transporting goods. It's your mission, if you accept the challenge, to
 build roads so that the robots can travel fast enough to satify the
 supply chain.
 
-## Changelog
-
-* The robots travel randomly between sites, transporing goods
-* Robots selects destination based on their cargo.
-
 ## Todo
 
 * Robots should avoid obstacles
+* Player should be able to build roads
+* Robots should move faster on roads
+* Robots should optimise to fastest route
+* Sites should display current stock
+
+
+## Changelog
+
+### 2/2 10:10
+* Sites should keep stock of goods
+
+### 2/2 8:00
+* Robots selects destination based on their cargo.
+
+### 1/2
+* The robots travel randomly between sites, transporing goods
--- lib/SystemLoader.rb
+++ lib/SystemLoader.rb
@@ -6,16 +6,21 @@ class SystemLoader < System
       load = @manager.get_component(e, CLoad)
       sprite = @manager.get_component(e, CSprite)
       yard = [target.x, target.y]
-      if target.speed == 0 && map.yards[yard]
-        if load.cargo
-          if load.cargo == map.yards[yard][:consume]
-            load.cargo=nil
-            sprite.animation = :empty
-          end
-        else
-          if map.yards[yard][:produce]
-            load.cargo = map.yards[yard][:produce]
-            sprite.animation = map.yards[yard][:produce]
+      next unless target.speed == 0 && map.yards[yard]
+      if load.cargo
+        if load.cargo == map.yards[yard][:consume]
+          map.yards[yard][:stock][load.cargo] += 1
+          load.cargo=nil
+          sprite.animation = :empty
+        end
+      else
+        yard = map.yards[yard]
+        prod = yard[:produce]
+        if prod
+          if yard[:stock][prod] && yard[:stock][prod] > 0
+            load.cargo = prod
+            yard[:stock][prod] -= 1
+            sprite.animation = prod
           end
         end
       end
