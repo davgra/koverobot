5c89fbf8e91e0fdd318208018f71dda2461b43f5
David Granath <dur@musiker.nu>
2013-02-07 15:48:53 +0100

    edit to match new speed

--- spec/SystemMove_spec.rb
+++ spec/SystemMove_spec.rb
@@ -5,45 +5,50 @@ require File.dirname(__FILE__) + "/../lib/entity_manager.rb"
 describe SystemMove do
   before(:all) do
     @manager = EntityManager.new
-      e= @manager.create_entity
-      @manager.add_component(e, CPosition.new(1,2))
-      @manager.add_component(e, CPath.new([:right,:down,:down,:left,:up]))
-      @manager.add_component(e, CTarget.new(2,2,0.01))
-      @manager.add_component(e, CSprite.new(nil,:stop))
 
-      @sys=SystemMove.new(@manager)
-    end
-    it "should move entity" do
-      @sys.game_tick(nil,1000/50.0)
-      pos=@manager.component(1, CPosition)
-      t = @manager.component(1, CTarget)
-      pos.x.should == 1.2
-      pos.y.should == 2.0
-      t.x.should == 2
-      t.y.should == 2
-    end
-    it "should stop on target" do
-      5.times do
-        @sys.game_tick(nil,20)
-      end
-      pos=@manager.component(1, CPosition)
-      "x: #{pos.x} y: #{pos.y}".should == "x: 2.0 y: 2.0"
-    end
-    it "should continue after target" do
-        @sys.game_tick(nil,20)
-      pos=@manager.component(1, CPosition)
-      "x: #{pos.x} y: #{pos.y}".should == "x: 2.2 y: 2.0"
-      target = @manager.component(1, CTarget)
-      target.x.should == 3
-      target.y.should == 2
-    end
-    it "should target" do
-      25.times do
-        @sys.game_tick(nil,20)
-      end
-      pos=@manager.component(1, CPosition)
-      "x: #{pos.x} y: #{pos.y}".should == "x: 2.0 y: 3.0"
-      sprite = @manager.component(1,CSprite)
-      sprite.direction.should == :stop_up
-    end
+    e= @manager.create_entity
+    @manager.add_component(e, CPosition.new(1,2))
+    @manager.add_component(e, CPath.new([:right,:down,:down,:left,:up]))
+    @manager.add_component(e, CTarget.new(2,2,0.01))
+    @manager.add_component(e, CSprite.new(nil,:stop))
+
+    e= @manager.create_entity :map
+    map = CMap.new([11,8])
+    @manager.add_component(e, map)
+
+    @sys=SystemMove.new(@manager)
   end
+  it "should move entity" do
+    @sys.game_tick(nil,1000/50.0)
+    pos=@manager.component(1, CPosition)
+    t = @manager.component(1, CTarget)
+    pos.x.should == 1.06
+    pos.y.should == 2.0
+    t.x.should == 2
+    t.y.should == 2
+  end
+  it "should stop on target" do
+    5.times do
+      @sys.game_tick(nil,20)
+    end
+    pos=@manager.component(1, CPosition)
+    "x: #{pos.x} y: #{pos.y}".should == "x: 1.3600000000000003 y: 2.0"
+  end
+  it "should continue after target" do
+    @sys.game_tick(nil,20)
+    pos=@manager.component(1, CPosition)
+    "x: #{pos.x} y: #{pos.y}".should == "x: 1.4200000000000004 y: 2.0"
+    target = @manager.component(1, CTarget)
+    target.x.should == 2
+    target.y.should == 2
+  end
+  it "should target" do
+    50.times do
+      @sys.game_tick(nil,20)
+    end
+    pos=@manager.component(1, CPosition)
+    "x: #{pos.x} y: #{pos.y}".should == "x: 3.0 y: 3.3600000000000003"
+    sprite = @manager.component(1,CSprite)
+    sprite.direction.should == :down
+  end
+end
